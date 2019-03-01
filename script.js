3cf5bde6749ca2ae61000a46fd2fb100c503cff0
Lafiagi <skillfullmallam@gmail.com>
2019-03-01 22:32:24 +0100

    Initial Commit For Login Page

---
+++ script.js
@@ -0,0 +1,10 @@
+if (document.title === "Login") {
+	let container = document.querySelector("#container");
+	container.style.height = "350px";
+
+}
+if (document.title === "Password Recovery") {
+	document.querySelector("#container").style.maxHeight="250px";
+	document.querySelector("#aligner").style.marginTop="40px";
+	//container.style.maxHeight="10px";
+}
